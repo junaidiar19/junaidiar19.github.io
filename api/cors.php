11e3421914ac37ce83c5bf8bd5e7fda3ea14a674
Junaidi Abdul Rahman <junaidiar.fcb@gmail.com>
2021-10-08 00:16:13 +0800

    update index

---
+++ api/cors.php
@@ -0,0 +1,4 @@
+<?php 
+    header('content-type: application/json; charset=utf-8');
+    header("Access-Control-Allow-Origin: *");
+?>
--- api/skill.php
+++ api/skill.php
@@ -1,8 +1,6 @@
 <?php 
 
-header("Access-Control-Allow-Headers: Authorization, Content-Type");
-header("Access-Control-Allow-Origin: *");
-header('content-type: application/json; charset=utf-8');
+include 'cors.php';
 
 $data = [
     [
