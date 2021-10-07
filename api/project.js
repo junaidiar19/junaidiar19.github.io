3ac5d69fbf3bd6148f9cf2ec39a045be27c11ad7
Junaidi Abdul Rahman <junaidiar.fcb@gmail.com>
2021-10-07 23:38:11 +0800

    update project

---
+++ api/project.js
@@ -0,0 +1,200 @@
+[
+    {
+        "name": "Online Learning Bersertifikat Guruinovatif.id",
+        "based": "Web Based",
+        "technology": "Laravel, Bootstrap",
+        "image": "images/project/gi.PNG"
+    },
+    {
+        "name": "Simple Course Microservices Architecture",
+        "based": "Web Based",
+        "technology": "Laravel, React JS, Express JS, MySQL, Netlify",
+        "image": "images/project/simple-course.PNG"
+    },
+    {
+        "name": "Sekolah Juara for Guruinovatif.id",
+        "based": "Web Based",
+        "technology": "Laravel, Architect UI, MySQL",
+        "image": "images/project/sekolah-juara-gi.PNG"
+    },
+    {
+        "name": "Learning Management System (LMS) The New You Institute",
+        "based": "Web Based",
+        "technology": "Laravel, Bootstrap, MySQL",
+        "image": "images/project/lms-tnyi.PNG"
+    },
+    {
+        "name": "Official Website Kampus Politeknik Hasnur (polihasnur.ac.id)",
+        "based": "Web Based",
+        "technology": "Laravel, PHP, MySQL",
+        "image": "images/project/polihasnur.PNG"
+    },
+    {
+        "name": "Platform HAFECS Representatif",
+        "based": "Web Based",
+        "technology": "Laravel, Bootstrap, MySQL",
+        "image": "images/project/hreps.PNG"
+    },
+    {
+        "name": "HAFECS Online Course v2.0",
+        "based": "Web Based",
+        "technology": "Laravel 6, Ajax, MySQL",
+        "image": "images/project/guruinovatif.PNG"
+    },
+    {
+        "name": "GIBS Teacher Assessment Application",
+        "based": "Web Based",
+        "technology": "CodeIgniter, Jquery, MySQL",
+        "image": "images/project/assessment.PNG"
+    },
+    {
+        "name": "Landing page website GampangJualan.id",
+        "based": "Web Based",
+        "technology": "CodeIgniter, basic-opl template, Ajax, MySQL",
+        "image": "images/project/gampangjualan.PNG"
+    },
+    {
+        "name": "Aplikasi Evoting v2.0 (Calon dan Pasangan Calon)",
+        "based": "Web Based",
+        "technology": "CodeIgniter, Jquery, Ajax, MySQL",
+        "image": "images/project/evoting2.PNG"
+    },
+    {
+        "name": "Sistem Informasi Akademik SMAN 1 Jenjangkit",
+        "based": "Web Based",
+        "technology": "CodeIgniter, Admin LTE, MySQL",
+        "image": "images/project/siakad-sman1jejangkit.PNG"
+    },
+    {
+        "name": "Website Penjualan Dealer Yamaha CV. Surya Prima Banjarmasin",
+        "based": "Web Based",
+        "technology": "CodeIgniter, Admin LTE, MySQL",
+        "image": "images/project/dealer yamaha.PNG"
+    },
+    {
+        "name": "Aplikasi Rekam Medis Puskesmas Tambarangan, Tapin",
+        "based": "Web Based",
+        "technology": "CodeIgniter, Admin LTE, MySQL",
+        "image": "images/project/rekammedis.PNG"
+    },
+    {
+        "name": "Aplikasi Pendataan Keluar Masuk Batubara CV. Binuang Mitra Mandiri",
+        "based": "Web Based",
+        "technology": "CodeIgniter, Admin LTE, MySQL",
+        "image": "images/project/batubara.PNG"
+    },
+    {
+        "name": "Sistem Pendukung Keputusan Metode Simple Additive Weighting (SAW)",
+        "based": "Web Based",
+        "technology": "CodeIgniter, Andora Template, MySQL",
+        "image": "images/project/spk-saw.PNG"
+    },
+    {
+        "name": "Aplikasi E-Voting Pemilihan (Ketua Osis, Kepala Desa, Ketua  BEM)",
+        "based": "Web Based",
+        "technology": "CodeIgniter, Admin LTE, MySQL",
+        "image": "images/project/Home User Evoting.PNG"
+    },
+    {
+        "name": "Sistem E-Learning Politeknik Hasnur (Ujian Online, Upload Tugas, Repository Materi)",
+        "based": "Web Based",
+        "technology": "CodeIgniter, Materialize CSS, MySQL",
+        "image": "images/project/E-Learning.PNG"
+    },
+    {
+        "name": "Aplikasi Perpustakaan SMAN 12 Banjarmasin",
+        "based": "Web Based",
+        "technology": "CodeIgniter, Admin LTE, MySQL",
+        "image": "images/project/Perpustakaan.PNG"
+    },
+    {
+        "name": "Sistem Pendataan Arsip Surat Menyurat di PDAM Kuala Kapuas",
+        "based": "Web Based",
+        "technology": "CodeIgniter, Andora Template, MySQL",
+        "image": "images/project/arsipsurat.PNG"
+    },
+    {
+        "name": "Website Penjualan Bahan Elektronik",
+        "based": "Web Based",
+        "technology": "CodeIgniter, PHP, Bootstrap, MySQL",
+        "image": "images/project/Elektro.PNG"
+    },
+    {
+        "name": "Aplikasi Kepegawaian dan Jadwal Mengajar SMAN 12 Banjarmasin",
+        "based": "Web Based",
+        "technology": "CodeIgniter, Bootstrap, MySQL",
+        "image": "images/project/Jadwal Mengajar.PNG"
+    },
+    {
+        "name": "Aplikasi Manajemen Kurikulum 2013 SDN Pasar Lama 6 Banjarmasin",
+        "based": "Web Based",
+        "technology": "CodeIgniter, Materialize CSS, MySQL",
+        "image": "images/project/Manajemen K13.PNG"
+    },
+    {
+        "name": "Website Penjualan Baju Fashion Pria",
+        "based": "Web Based",
+        "technology": "CodeIgniter, Bootstrap, MySQL",
+        "image": "images/project/home fashion.PNG"
+    },
+    {
+        "name": "Aplikasi Request Digitaliz Yayasan Hasnur Centre",
+        "based": "Web Based",
+        "technology": "CodeIgniter, Bootstrap, MySQL",
+        "image": "images/project/req-digi.PNG"
+    },
+    {
+        "name": "Aplikasi Manajemen Penjualan Bahan Sparepart PT. Rezeki Cahya AMHA Banjarmasin",
+        "based": "Web Based",
+        "technology": "CodeIgniter, Bootstrap, MySQL",
+        "image": "images/project/Aplikasi Penjualan.PNG"
+    },
+    {
+        "name": "Sistem Pendukung Keputusan Kepala Desa Menggunakan Metode WP (Weighted Product)",
+        "based": "Web Based",
+        "technology": "CodeIgniter, Bootstrap, MySQL",
+        "image": "images/project/SPK.PNG"
+    },
+    {
+        "name": "Aplikasi Manajemen Penjualan Bahan Bangunan Toko Mentaya Banjarmasin",
+        "based": "Web Based",
+        "technology": "SMS Gateway, CodeIgniter, Materialize CSS",
+        "image": "images/project/Toko Bahan Bangunan.PNG"
+    },
+    {
+        "name": "Website Penerimaan Mahasiswa Baru Politeknik Hasnur",
+        "based": "Web Based",
+        "technology": "Laravel, Dashboard Admin with Materialize CSS",
+        "image": "images/project/pmb-daftar.PNG"
+    },
+    {
+        "name": "Aplikasi Perpustakaan Sederhana (Untuk Referensi Pembelajaran)",
+        "based": "Web Based",
+        "technology": "CodeIgniter, Bootstrap 4, MySQL",
+        "image": "images/project/perpus-simple.PNG"
+    },
+    {
+        "name": "Aplikasi Pemilihan Kepala Desa Baruta Analaki, Sulawesi Tenggara",
+        "based": "Web Based",
+        "technology": "Login with - QR Code Scan, CodeIgniter",
+        "image": "images/project/pilkades.PNG"
+    },
+    {
+        "name": "Aplikasi Manajemen Data Cafe (Absensi, Penjualan, Penggajian)",
+        "based": "Web Based",
+        "technology": "CodeIgniter, Bootstrap 4, SB Admin Template",
+        "image": "images/project/ci-cafe.PNG"
+    },
+    {
+        "name": "Aplikasi Manajemen Penjualan Toko Baju Dill Surf",
+        "based": "Web Based",
+        "technology": "CodeIgniter, Bootstrap 4, MySQL",
+        "image": "images/project/dill-surf.PNG"
+    },
+    {
+        "name": "User Interface Design for Adver.id",
+        "based": "Web Based",
+        "technology": "Figma",
+        "image": "images/project/adver-figma.PNG"
+    }
+]
